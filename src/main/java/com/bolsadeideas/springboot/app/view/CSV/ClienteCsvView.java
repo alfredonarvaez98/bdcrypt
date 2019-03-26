5390a76bdb7c61fee10d14d88c4999e5c2eb963c
Fredy1905 <alfredonarva98@gmail.com>
2019-03-26 12:10:52 +0100

    Exportar a CSV completado

--- src/main/java/com/bolsadeideas/springboot/app/view/CSV/ClienteCsvView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/CSV/ClienteCsvView.java
@@ -1,8 +1,13 @@
 package com.bolsadeideas.springboot.app.view.CSV;
 
 
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,12 +26,24 @@ public class ClienteCsvView extends AbstractView {
         return true;
     }
 
+
+    @SuppressWarnings("unchecked")
     @Override
     protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest httpServletRequest, HttpServletResponse response) throws Exception {
 
     response.setHeader("Content-Disposition","attachment; filename=\"clientes.csv\"");
     response.setContentType(getContentType());
 
+        Page<Cliente> clientes = (Page<Cliente>) model.get("clientes");
+        ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(),  CsvPreference.STANDARD_PREFERENCE);
 
+        String[] header = {"id", "nombre", "apellido", "email", "createAt"};
+
+        for(Cliente cliente: clientes) {
+            beanWriter.write(cliente, header);
+        }
+
+        beanWriter.close();
     }
+
 }
